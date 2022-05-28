5449143bf7d81aa32b1a989af52fc0369c5a692c
Efremov Andrei <cantidepunishment@gmail.com>
2022-05-28 23:12:19 +0900

    skills, GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject enemy;
+    public TMP_Text text;
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = enemy.GetComponentInChildren<TMP_Text>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/SkillDex.cs
+++ Assets/Scripts/SkillDex.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class SkillDex : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // public enum 
+    private class SkillEffect
     {
-        
-    }
+        public string Target { get; set;}
+        public int Damage { get; set;}
+        public int Heal { get; set;}
+        public int Accuracy { get; set;}
+        public int Priority { get; set;}
+        public int Speed { get; set;}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
+    private class SkillDexInfo
+    {
+        public string Name { get; set;}
+        public string Type { get; set;}
+        public List<SkillEffect> Effects { get; set;}
+    }
+    Dictionary<int, SkillDexInfo> MokeDexDict = new Dictionary<int, SkillDexInfo>
+    {
+        {
+            025, new SkillDexInfo { Name = "Quick Attack", Type = "Normal", Effects = new List<SkillEffect>(){new SkillEffect{Target = "Enemy", Damage = 40, Accuracy = 100, Priority = 1}}}
+        }
+    };
 }
