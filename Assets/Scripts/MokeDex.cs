0fc2d864f539fe5e9a5502aaf826ee59f2637cb6
Novgorodov Tit <novgorodov.tit@opus.co.jp>
2022-05-28 21:30:52 +0900

    added MokeDex dict

--- Assets/Scripts/MokeDex.cs
+++ Assets/Scripts/MokeDex.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class MokeDex : MonoBehaviour
 {
+    private class MokeStats
+    {
+        public int HP { get; set;}
+        public int Attack { get; set;}
+        public int Defense { get; set;}
+        public int SpAtk { get; set;}
+        public int SpDef { get; set;}
+        public int Speed { get; set;}
+
+    }
+    private class MokeDexInfo
+    {
+        public string Name { get; set;}
+        public string Type { get; set;}
+        public MokeStats Stats { get; set;}
+    }
+    Dictionary<int, MokeDexInfo> MokeDexDict = new Dictionary<int, MokeDexInfo>
+    {
+        {
+            025, new MokeDexInfo { Name = "Pikachu", Type = "Electric", new MokeStats {HP = 35, Attack = 55, Defense = 40, SpAtk = 50, SpDef = 50, Speed = 90}}
+        }
+    };
     // Start is called before the first frame update
     void Start()
     {
