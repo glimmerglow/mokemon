9d10a265ae90b027b88daaeff780d06a75d22ab5
Efremov Andrei <cantidepunishment@gmail.com>
2022-05-29 15:21:13 +0900

    skill ui

--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skill
+public class Skill : MonoBehaviour
 {
-    
+    public void UseSkill()
+    {
+        
+    }
 }
